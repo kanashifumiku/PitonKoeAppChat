5e642d18255fbc07044e444222b5e38b7ebee572
pinopinopinopinopinopinopinopinopinopinopinopino <a360id@gmail.com>
2025-12-25 06:18:04 -0800

    Add files via upload
    Upgrade Fitur

--- Klien.py
+++ Klien.py
@@ -1,36 +1,67 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import messagebox
+from tkinter import simpledialog
 import socket
 import threading
 import time
 import datetime
 
+def awal():
+    print('Klien Perpesanan Lokal (Pesan Privat)')
+    while True:
+        client_id = simpledialog.askstring('Pengguna PesanKoe | Masuk', 'Masukkan ID Pengguna',)
+        if client_id:
+            break
+        else:
+            galat_id = messagebox.askretrycancel("Galat", "ID Pengguna tidak boleh kosong. Masukkan ulang ID?")
+            if not galat_id:
+                messagebox.showwarning('Perhatian', 'Program akan ditutup')
+                return
+    
+    while True:
+        host = simpledialog.askstring('Pengguna PesanKoe | Masuk', 'Masukkan Peladen')
+        if host:
+            break
+        else:
+            galat_host = messagebox.askretrycancel('Galat', 'Peladen tidak boleh kosong. Masukkan ulang Peladen?')
+            if not galat_host:
+                messagebox.showinfo('Perhatian', 'Program akan ditutup')
+                return
+                
+    messagebox.showinfo('Selamat Datang', f'Hai, {client_id}.\nPetunjuk Penggunaan:\n1. Ketik all pada ID untuk pesan BROADCAST\n2. Jika Peladen tidak bisa tersambung, mulai ulang program')
+    root = tk.Tk()
+    app = ChatClientGUI(root, client_id)
+    app.connect_to_server(host, 12345)
+    root.mainloop()
+
 class ChatClientGUI:
     def __init__(self, root, client_id):
         self.root = root
         self.client_id = client_id
-        self.root.title(f'Chat Klien - {client_id}')
-        self.root.geometry('600x450')
+
+        self.root.title(f'Pengguna PesanKoe | ID: {client_id}')
+        root.resizable(False, False) # Agar window tidak bisa diresize
+        self.root.geometry('400x600')
+
         # Teks Area untuk Log Chat
         self.text_area = scrolledtext.ScrolledText(root, height=20, width=70)
         self.text_area.pack(padx=10, pady=10)
+
         # Frame untuk Input
         self.input_frame = tk.Frame(root)
-        self.input_frame.pack(pady=5)
+        self.input_frame.pack(padx=10, pady=5, fill="x")
+
         # Label dan input untuk ID Penerima
-        tk.Label(self.input_frame, text='Kepada (ID):').pack(side=tk.LEFT, padx=5)
+        tk.Label(self.input_frame, text='Kepada (ID):').pack(side=tk.TOP, padx=5, anchor='w')
         self.target_entry = tk.Entry(self.input_frame, width=20)
-        self.target_entry.pack(side=tk.LEFT, padx=5)
+        self.target_entry.pack(side=tk.TOP, padx=5, anchor='w')
+
         # Input untuk pesan
-        tk.Label(self.input_frame, text='Pesan:').pack(side=tk.LEFT, padx=5)
+        tk.Label(self.input_frame, text='Pesan:').pack(side=tk.TOP, padx=5, anchor='w')
         self.message_entry = tk.Entry(self.input_frame, width=30)
-        self.message_entry.pack(side=tk.LEFT, padx=5)
-        # Tombol untuk koneksi 
-        self.server_entry = tk.Entry(root, width=20)
-        self.server_entry.pack(pady=5)
-        self.server_entry.insert(0, 'server')
-        self.connect_button = tk.Button(root, text='Hubungkan ke Peladen', command=self.connect_to_server_gui)
-        self.connect_button.pack(padx=5)
+        self.message_entry.pack(side=tk.TOP, padx=5, anchor='w')
+
         self.client_socket = None
         self.log_file = f'klien_{client_id}_log.txt'
         self.running = True
@@ -49,10 +80,10 @@ class ChatClientGUI:
             try:
                 data = self.client_socket.recv(1024).decode('ascii')
                 if not data:
-                    self.log_message('Server mengakhiri koneksi')
+                    self.log_message('[SISTEM] Server mengakhiri koneksi')
                     self.running = False
                     break
-                self.log_message(f'Diterima: {data}')
+                self.log_message(f'{data}')
             except Exception as e:
                 self.log_message(f'Galat menerima: {e}')
                 self.running = False
@@ -65,11 +96,18 @@ class ChatClientGUI:
         if target_id and message and self.client_socket:
             try:
                 start_time = time.time()
-                full_message = f'TO:{target_id}:{message}'
-                self.client_socket.send(full_message.encode('ascii'))
+                if target_id.upper() == 'ALL':
+                    full_msg = f'ALL:{message}'
+                else:
+                    full_msg = f'TO:{target_id}: {message}'
+                # full_message = f'TO:{target_id}:{message}'
+                self.client_socket.send(full_msg.encode('ascii'))
                 rtt = (time.time() - start_time) * 1000 # satuan MS
                 ascii_codes = [ord(char) for char in message]
-                self.log_message(f'Terkirim ke {target_id}: {message} (ASCII: {ascii_codes}, RTT: {rtt:.2f} ms)')
+                if target_id.upper() == 'ALL':
+                    self.log_message(f'[BROADCAST --> ALL]: {message} (ASCII: {ascii_codes}, RTT: {rtt:.2f} ms)')
+                else:
+                    self.log_message(f'[PRIVAT --> {target_id}] {message} (ASCII: {ascii_codes}, ) || RTT: {rtt:.2f} ms')
                 if message.lower() == 'exit':
                     self.running = False
                     self.client_socket.close()
@@ -80,10 +118,6 @@ class ChatClientGUI:
                 self.log_message(f'Galat mengirim: {e}')
         else:
             self.log_message('Galat: Isi ID Penerima dan Pesan')
-
-    def connect_to_server_gui(self):
-        host = self.server_entry.get()
-        self.connect_to_server(host, 12345)
     
     def connect_to_server(self, host, port=12345):
         # Menghubungkan ke Peladen
@@ -92,15 +126,15 @@ class ChatClientGUI:
             self.client_socket.connect((host, port))
             self.client_socket.send(self.client_id.encode('ascii')) # Utk Kirim ID Klien
             self.log_message(f'Terhubung ke {host}:{port}')
-            self.connect_button.config(state='disabled')
+            # self.connect_button.config(state='disabled')
             self.message_entry.bind('<Return>', self.send_message) # Even -driven: Enter utk Kirim
             threading.Thread(target=self.receive_messages, daemon=True).start()
+            messagebox.showinfo('Informasi', f'Berhasil terhubung ke Peladen\n{host}:{port}')
         except Exception as e:
             self.log_message(f'Galat Koneksi: {e}')
+            galat_konek = messagebox.showerror('Galat Koneksi', f'Tidak bisa connect ke {host}:{port}\n\n{e}')
+            self.root.destroy()
+                
 
 if __name__ == '__main__':
-    print('Klien Perpesanan Lokal (Pesan Privat)')
-    client_id = input('Masukkan ID Klien. Misalnya Klien1\n> ')
-    root = tk.Tk()
-    app = ChatClientGUI(root, client_id)
-    root.mainloop()
+    awal()
--- Peladen.py
+++ Peladen.py
@@ -2,26 +2,27 @@ import socket
 import threading
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import messagebox
 import datetime
 
 class ChatServerGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title('Chat Peladen - Lokal (Pesan Privat)')
-        self.root.geometry('600x600')
-        # Teks Area untuk log Peladen
-        self.text_area = scrolledtext.ScrolledText(root, height=20, width=70)
-        self.text_area.pack(padx=10, pady=10)
-        # Tombol mulai peladen
-        self.start_button = tk.Button(root, text='Mulai Peladen', command=self.start_server_gui)
-        self.start_button.pack(pady=5)
+        self.root.title('Peladen PesanKoe') # Judul window
+        root.resizable(False, False) # Agar window tidak bisa diresize 
+        self.text_area = scrolledtext.ScrolledText(root, height=10, width=50) # Teks Area untuk log Peladen
+        self.text_area.pack(padx=5, pady=5, side=tk.BOTTOM)
         self.server_socket = None
-        self.clients = [] # Daftar (socket, klien_id)
+        self.clients = {} # Daftar (socket, klien_id) pake DICT
         self.client_lock = threading.Lock() # Lock untuk sinkron
-        self.log_file = 'LogPeladen.txt'
-        self.host_entry = tk.Entry(root, width=30) # Tombol untuk input Host
-        self.host_entry.pack(pady=5)
+        self.top = tk.Frame(root)
+        self.top.pack(padx=10, pady=5, fill="x")
+        self.host_entry = tk.Entry(self.top, width=30) # Entry untuk input Host biar dinamis
+        self.host_entry.pack(pady=5, side=tk.TOP, anchor='w')
         self.host_entry.insert(0, "Masukkan IP Server Peladen")
+        self.start_button = tk.Button(self.top, text='Mulai Peladen', command=self.start_server_gui)  # Tombol mulai peladen
+        self.start_button.pack(pady=5, side=tk.TOP, anchor='w')
+        self.log_file = 'Peladen.txt'
 
     def log_message(self, message):
         # Catat Pesan ke Berkas Log
@@ -31,55 +32,84 @@ class ChatServerGUI:
         with open(self.log_file, 'a') as f:
             f.write(f'[{timestamp}] {message}\n')
 
-    def send_private_message(self, message, source_socket, target_id):
+    def send_broadcast(self, message, sender_id): # Kirim Broadcast
+        with self.client_lock:
+            sent_count = 0
+            for client_id, client_socket in self.clients.items(): 
+                if client_id != sender_id:
+                    try:
+                        brd_msg = f'[BROADCAST] {message}'
+                        client_socket.send(brd_msg.encode('ascii'))
+                        self.log_message(f'[BROADCAST] {sender_id} ke {client_id}: {message}')
+                        sent_count += 1
+                    except Exception as e:
+                        self.log_message(f'Galat kirim Ke {client_id}: {e}')
+            if sent_count > 0:
+                self.log_message(f'BROADCAST berhasil ke {sent_count} klien')
+            return sent_count > 0
+
+    def send_private_message(self, message, source_socket, target_id): # Kirim privat
         # Kirim Pesan Privat ke Klien dengan ID Tertentu
         with self.client_lock:
-            for client_socket, client_id in self.clients:
-                if client_id == target_id:
-                    try:
-                        client_socket.send(message.encode('ascii'))
-                        return True
-                    except Exception as e:
-                        self.log_message(f'Galat mengirim ke {target_id}: {e}')
-                        return False
-            return False
+            if target_id in self.clients:
+                client_socket = self.clients[target_id]
+                try:
+                     client_socket.send(message.encode('ascii'))
+                     return True
+                except Exception as e:
+                    self.log_message(f'Galat mengirim ke {target_id}: {e}')
+                    return False
+        return False
     
     def handle_client(self, client_socket, client_address, client_id):
         # Menangani Pesan dari Klien
-        self.log_message(f'Koneksi dari {client_address} (ID: {client_id})')
+        self.log_message(f'[SISTEM] Koneksi baru dari {client_address} (ID: {client_id})')
         with self.client_lock:
-            self.clients.append((client_socket, client_id))
+            self.clients[client_id] = client_socket #Dict
         try:
             while True:
                 data = client_socket.recv(1024).decode('ascii')
                 if not data or data.lower() == 'exit':
-                    self.log_message(f'Client {client_id} mengakhiri koneksi')
+                    self.log_message(f'[SISTEM] Client {client_id} mengakhiri koneksi')
                     break
-                # Parse pesan degan format TO:<target_id>:<message>
-                if data.startswith('TO:'):
+                # Parse pesan dengan format yang sudah ditentukan
+                if data.startswith('TO:'): # Ini Untuk Pesan Privat
                     try:
                         _, target_id, message = data.split(':', 2)
                         ascii_codes = [ord(char) for char in message]
-                        log_meessage = f'Dari {client_id} ke {target_id}: {message} (ASCII: {ascii_codes})'
-                        self.log_message(log_meessage)
-                        success = self.send_private_message(log_meessage, client_socket, target_id)
+                        log_message = f'[PRIVAT {client_id} --> {target_id}] {message} (ASCII: {ascii_codes})'
+                        self.log_message(log_message)
+                        success = self.send_private_message(log_message, client_socket, target_id)
                         if not success:
                             try:
-                                client_socket.send(f'Galat: Klien {target_id} tak ditemukan'.encode('ascii'))
+                                client_socket.send(f'[SISTEM] Galat: Klien {target_id} tak ditemukan'.encode('ascii'))
                             except:
                                 pass
                     except ValueError:
-                        self.log_meessage(f'Galat Pada Format Pesan dari {target_id}: {data}')
-                        client_socket.send('Galat: Format Pesan Salah (TO:<target_id>:<message>)'.encode('ascii'))
+                        self.log_message(f'[SISTEM] Galat Pada Format Pesan dari {target_id}: {data}')
+                        client_socket.send('[SISTEM] Galat: Format Pesan Salah (TO:<target_id>:<message>)'.encode('ascii'))
+                elif data.startswith('ALL:'): # Ini untuk pesan broadcast
+                    try:
+                        broadcast_msg = data[4:].strip()  # Hilangkan "ALL:"
+                        if broadcast_msg:
+                            success = self.send_broadcast(broadcast_msg, client_id)
+                            if not success:
+                                messagebox.showwarning('Pemberitahuan', f'Tidak ada pengguna lain untuk menerima broadcast anda')
+                                client_socket.send('[SISTEM] Galat: Tidak ada client lain untuk broadcast'.encode('ascii'))
+                        else:
+                            client_socket.send('[SISTEM] Galat: Pesan broadcast kosong'.encode('ascii'))
+                    except Exception as e:
+                        self.log_message(f'[SISTEM] Galat broadcast {client_id}: {e}')
+                        client_socket.send('Galat: Format broadcast salah (ALL:message)'.encode('ascii'))
                 else:
-                    self.log_message(f'Galat: Pesan Dari {client_id} tidak menggunakan format TO:<target_id:<message>')
+                    self.log_message(f'[SISTEM] Galat: Pesan Dari {client_id} tidak menggunakan format TO:<target_id:<message>')
                     client_socket.send('Galat: Gunakan Format TO:<target_id>:<message>'.encode('ascii'))
         except Exception as e:
-            self.log_message(f'Galat pada Klien {client_id}: {e}')
+            self.log_message(f'[SISTEM] Galat pada Klien {client_id}: {e}')
         finally:
             with self.client_lock:
-                self.clients = [(cs, cid) for cs, cid in self.clients if cs != client_socket]
-            client_socket.close()
+                if client_id in self.clients:
+                    del self.clients[client_id]
     
     def accept_connections(self):
         # Menerima koneksi dari klien
@@ -91,7 +121,7 @@ class ChatServerGUI:
             except:
                 break
 
-    def start_server_gui(self):
+    def start_server_gui(self): # Custom host input using GUI
         host = self.host_entry.get()
         self.start_server(host, 12345)
     
@@ -102,11 +132,13 @@ class ChatServerGUI:
             self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server_socket.bind((host, port))
             self.server_socket.listen(5)
-            self.log_message(f'Peladen berjalan di {host}:{port}')
+            self.log_message(f'[SISTEM] Peladen berjalan di {host}:{port}')
             self.start_button.config(state='disabled')
             threading.Thread(target=self.accept_connections, daemon=True).start()
+            messagebox.showinfo('Ingfo', f'Peladen berhasil diaktifkan\n{host}:{port}')
         except Exception as e:
-            self.log_message(f'Galat saat memulai Peladen: {e}')
+            messagebox.showerror('Galat', f'{e}')
+            self.log_message(f'[SISTEM] Galat saat memulai Peladen: {e}')
 
 if __name__ == '__main__':
     print('Peladen Perpesanan Lokal (Pesan Privat)')
